40e12c16880968048e2c99d7f5b701afa35b610e
ezzeldem <ezzelden06@gmail.com>
2022-07-24 14:00:55 +0200

    Created a new Expo app

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.screen}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
